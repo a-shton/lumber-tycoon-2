3f1237deb6207dde0ed019259518cc129fa8d955
Lua <luahubexploits@gmail.com>
2022-01-12 20:36:02 +0000

    Create launcher.lua

---
+++ launcher.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == "2248408710" then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/lua-ware/luaware/main/destructionsimulator.lua'), true)()
+elseif game.PlaceId == "292439477"
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/lua-ware/luaware/main/phantomforces.lua'), true)()
+end
