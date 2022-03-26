96d69729f5c02a8b2e878ce9be09abc55e33bb1b
Lua <luahubexploits@gmail.com>
2022-03-26 13:10:04 +0000

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -4,5 +4,8 @@ end)
 
 if game.PlaceId == 1962086868 then
   print("Luaware | Tower of Hell")
-  
+  load("https://raw.githubusercontent.com/lua-ware/luaware/main/towerofhell.lua")
+else if game.PlaceId == 2248408710 then
+  print("Luaware - Destruction Simulator")
+  load("https://raw.githubusercontent.com/lua-ware/luaware/main/destructionsimulator.lua")
 end
